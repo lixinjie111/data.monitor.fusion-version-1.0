11cef450c571a969e27296bae063c0e6e70d4045
yuyifang <yu.yf@foxmail.com>
2019-08-09 10:10:33 +0800

    no message

--- src/components/Tusvn3DMap2.vue
+++ src/components/Tusvn3DMap2.vue
@@ -58,12 +58,13 @@ export default {
             ,cartrackwebsocketUrl:"ws://120.133.21.14:49982/mon"
             ,carid:'B21E-00-024'
             ,carTrackAction:'fusel_sider_veh'//fusel_sider_veh track
-            ,pmodels:{
-                "2-2":new Array(),
-                "2-1":new Array(),
-                "1-1":new Array(),
-                "1-2":new Array()
-            }//旁车模型
+            // ,pmodels:{
+            //     "2-2":new Array(),
+            //     "2-1":new Array(),
+            //     "1-1":new Array(),
+            //     "1-2":new Array()
+            // }//旁车模型
+            ,pmodels:[]
             ,pCacheModelNum:100
 
             ,matStdObjects : new THREE.MeshStandardMaterial( { color: 0x7337E3, roughness: 1, metalness: 0, opacity: 0.7, transparent: true } )
@@ -726,15 +727,11 @@ export default {
             let  pcars = json.result.data;
             if(pcars!=null)
             {
-                // debugger
-                if(_.size(this.pmodels["2-2"])==0)
+                if(this.pmodels.length == 0)
                 {
-                    // this.pmodels = [];
                     for(let m=0;m<this.pCacheModelNum;m++)
                     {
-                        //2-2
                         let mesh1 = new THREE.MeshStandardMaterial( { color: 0xab6604, roughness: 1, metalness: 0, opacity: 0.7, transparent: true } );
-                        // let dUTM = proj4(this.sourceProject,this.destinatePorject,[pcar.longitude,pcar.latitude]);
                         var geoBox1 = new THREE.BoxBufferGeometry(1.7, 4.6, 1.4);
                         var model1 = new THREE.Mesh( geoBox1, mesh1);
                         model1.position.set(0,0, this.defualtZ );
@@ -742,136 +739,92 @@ export default {
                         model1.castShadow = true;
                         model1.receiveShadow = true;
                         this.scene.add(model1);
-                        this.pmodels["2-2"].push(model1);
+                        this.pmodels.push(model1);
+                    }
+                }
+                for(let i=0;i<this.pmodels.length;i++)
+                {
+                    var model = this.pmodels[i];
+                     model.position.set( 0, 0, this.defualtZ );
+                }
 
-                        //2-1
-                        let mesh2 = new THREE.MeshStandardMaterial( { color: 0xffbf64, roughness: 1, metalness: 0, opacity: 0.7, transparent: true } );
-                        // let dUTM = proj4(this.sourceProject,this.destinatePorject,[pcar.longitude,pcar.latitude]);
-                        var geoBox2 = new THREE.BoxBufferGeometry(1.7, 4.6, 1.4);
-                        var model2 = new THREE.Mesh( geoBox2, mesh2);
-                        model2.position.set(0,0, this.defualtZ );
-                        model2.rotation.set( this.pitch,this.yaw,this.roll );
-                        model2.castShadow = true;
-                        model2.receiveShadow = true;
-                        this.scene.add(model2);
-                        this.pmodels["2-1"].push(model2);
-
-                        //1-1
-                        let mesh3 = new THREE.MeshStandardMaterial( { color: 0xa257d1, roughness: 1, metalness: 0, opacity: 0.7, transparent: true } );
-                        // let dUTM = proj4(this.sourceProject,this.destinatePorject,[pcar.longitude,pcar.latitude]);
-                        var geoBox3= new THREE.BoxBufferGeometry(1.7, 4.6, 1.4);
-                        var model3 = new THREE.Mesh( geoBox3, mesh3);
-                        model3.position.set(0,0, this.defualtZ );
-                        model3.rotation.set( this.pitch,this.yaw,this.roll );
-                        model3.castShadow = true;
-                        model3.receiveShadow = true;
-                        this.scene.add(model3);
-                        this.pmodels["1-1"].push(model3);
-
-                        //1-2
-                        let mesh4 = new THREE.MeshStandardMaterial( { color: 0x651399, roughness: 1, metalness: 0, opacity: 0.7, transparent: true } );
-                        // let dUTM = proj4(this.sourceProject,this.destinatePorject,[pcar.longitude,pcar.latitude]);
-                        var geoBox4= new THREE.BoxBufferGeometry(1.7, 4.6, 1.4);
-                        var model4 = new THREE.Mesh( geoBox4, mesh4);
-                        model4.position.set(0,0, this.defualtZ );
-                        model4.rotation.set( this.pitch,this.yaw,this.roll );
-                        model4.castShadow = true;
-                        model4.receiveShadow = true;
-                        this.scene.add(model4);
-                        this.pmodels["1-2"].push(model4);
-                    }
-                }
-                for(let n = 0;n<this.pmodels["2-2"].length;n++)
+                for(let n=0;n<pcars.length;n++)
                 {
-                    var pmodel1 = this.pmodels["2-2"][n];
-                    if(pmodel1!=null)
+                    let pcar = pcars[n];
+                    if(pcar.type!=1)
                     {
-                         pmodel1.position.set( 0, 0, this.defualtZ );
-                    }
-                }
-                for(let n = 0;n<this.pmodels["2-1"].length;n++)
-                {
-                    var pmodel2 = this.pmodels["2-1"][n];
-                    if(pmodel2!=null)
-                    {
-                         pmodel2.position.set( 0, 0, this.defualtZ );
-                    }
-                }
-                for(let n = 0;n<this.pmodels["1-1"].length;n++)
-                {
-                    var pmodel3 = this.pmodels["1-1"][n];
-                    if(pmodel3!=null)
-                    {
-                         pmodel3.position.set( 0, 0, this.defualtZ );
-                    }
-                }
-                for(let n = 0;n<this.pmodels["1-2"].length;n++)
-                {
-                    var pmodel4 = this.pmodels["1-2"][n];
-                    if(pmodel4!=null)
-                    {
-                         pmodel4.position.set( 0, 0, this.defualtZ );
-                    }
-                }
-                // for(let n=0;n<dl.models.length;n++)
-                // {
-                //     let m = dl.models[n];
-                //     dl.removeModel(m,this.viewer);
-                // }
-                let num_2_1=0;
-                let num_2_2=0;
-                let num_1_1=0;
-                let num_1_2=0;
-
-                for(let i=0;i<pcars.length;i++)
-                {
-                    let pcar = pcars[i];
-                    let dUTM = proj4(this.sourceProject,this.destinatePorject,[pcar.longitude,pcar.latitude]);
-                    if(pcar.fuselType==2&&pcar.fuselLevel==1)
-                    {
-                        let model = this.pmodels["2-1"][num_2_1];
+                        let dUTM = proj4(this.sourceProject,this.destinatePorject,[pcar.longitude,pcar.latitude]);
+                        let model = this.pmodels[n];
                         model.position.set(dUTM[0],dUTM[1], this.defualtZ );
                         model.rotation.set( this.pitch,this.yaw,(-Math.PI / 180) * pcar.heading);
-                        num_2_1++;
+                        this.changeModelColor(pcar,model);
+                        
+                        // if(pcar.fuselType==2&&pcar.fuselLevel==1)
+                        // {
+                        //     model.material.color.setStyle("#ffbf64");
+                        // }
+                        // if(pcar.fuselType==2&&pcar.fuselLevel==2)
+                        // {
+                        //     model.material.color.setStyle("#ab6604");
+                        // }
+                        // if(pcar.fuselType==1&&pcar.fuselLevel==1)
+                        // {
+                        //     model.material.color.setStyle("#a257d1");
+                        // }
+                        // if(pcar.fuselType==1&&pcar.fuselLevel==2)
+                        // {
+                        //     model.material.color.setStyle("#651399");
+                        // }
+                    }else{//type=1  平台注册的车
+                        let mesh1 = new THREE.MeshStandardMaterial( { color: 0xab6604, roughness: 1, metalness: 0, opacity: 0.7, transparent: true } );
+                        var geoBox1 = new THREE.BoxBufferGeometry(1.7, 4.6, 1.4);
+                        var model1 = new THREE.Mesh( geoBox1, mesh1);
+                        this.animateCar(pcar,model1);
+                        this.animateCar(pcar)
                     }
-
-                    if(pcar.fuselType==2&&pcar.fuselLevel==2)
-                    {
-                        let model = this.pmodels["2-2"][num_2_2];
-                        model.position.set(dUTM[0],dUTM[1], this.defualtZ );
-                        model.rotation.set( this.pitch,this.yaw,(-Math.PI / 180) * pcar.heading);
-                        num_2_2++;
-                    }
-
-                    if(pcar.fuselType==1&&pcar.fuselLevel==2)
-                    {
-                        let model = this.pmodels["1-2"][num_1_2];
-                        model.position.set(dUTM[0],dUTM[1], this.defualtZ );
-                        model.rotation.set( this.pitch,this.yaw,(-Math.PI / 180) * pcar.heading);
-                        num_1_2++;
-                    }
-                    if(pcar.fuselType==1&&pcar.fuselLevel==1)
-                    {
-                        let model = this.pmodels["1-1"][num_1_1];
-                        model.position.set(dUTM[0],dUTM[1], this.defualtZ );
-                        model.rotation.set( this.pitch,this.yaw,(-Math.PI / 180) * pcar.heading);
-                        num_1_1++;
-                    }
+                    
                 }
             }
-
-            
             // return ;
             //处理自车信息
             let data2 = json.result.selfVehInfo;
             // console.log(data2);
+            this.animateCar(data2);
+           
+        },
+        changeModelColor:function(data,model)
+        {
+            if(data.fuselType==2&&data.fuselLevel==1)
+            {
+                model.material.color.setStyle("#ffbf64");
+            }
+            if(data.fuselType==2&&data.fuselLevel==2)
+            {
+                model.material.color.setStyle("#ab6604");
+            }
+            if(data.fuselType==1&&data.fuselLevel==1)
+            {
+                model.material.color.setStyle("#a257d1");
+            }
+            if(data.fuselType==1&&data.fuselLevel==2)
+            {
+                model.material.color.setStyle("#651399");
+            }
+        },
+        animateCar:function(data2,model){
             if(data2!=null)
             {
                 let vid = data2.vehicleId;
                 let carModel = this.models[vid];
                 if(carModel==null)
                 {
-                    this.addModel(vid,"./static/map3d/map_photo/car.3DS",data2.longitude,data2.latitude,this.defualtZ);
+                    if(model==undefined)
+                    {
+                        this.addModel(vid,"./static/map3d/map_photo/car.3DS",data2.longitude,data2.latitude,this.defualtZ);
+                    }else{
+                        // debugger;
+                        this.models[vid]=model;
+                    }
                     // this.add3DInfoLabel(vid,vid,data2.longitude,data2.latitude,this.defualtZ);
                 }
 
@@ -888,8 +841,15 @@ export default {
                 if(this.carBusinessData[vid]['lastUtmPosition'] == null)
                 {
                     this.carBusinessData[vid]['utmposition'] = proj4(this.sourceProject,this.destinatePorject,[data2.longitude,data2.latitude]);
-                    this.models[vid].position.x = this.carBusinessData[vid]['utmposition'][0];
-                    this.models[vid].position.y = this.carBusinessData[vid]['utmposition'][1];
+                    if(this.models[vid] instanceof dl.Model)
+                    {
+                        this.models[vid].position.x = this.carBusinessData[vid]['utmposition'][0];
+                        this.models[vid].position.y = this.carBusinessData[vid]['utmposition'][1];
+                    }else{
+                        this.models[vid].position.set(this.carBusinessData[vid]['utmposition'][0],this.carBusinessData[vid]['utmposition'][1], this.defualtZ );
+                        this.changeModelColor(data2,this.models[vid]);
+                    }
+                    
 
                     // this.infoLabels["gan"][vid].position.set(this.carBusinessData[vid]['utmposition'][0],this.carBusinessData[vid]['utmposition'][1],this.defualtZ+4);
                     // this.infoLabels["label"][vid].setPositon([this.carBusinessData[vid]['utmposition'][0],this.carBusinessData[vid]['utmposition'][1],this.defualtZ+8]);
@@ -900,7 +860,12 @@ export default {
 
                     this.carBusinessData[vid]['utmposition'][2] = data2.heading;
                     this.carBusinessData[vid]['utmposition'][3] = data2.gpsTime;
-                    this.models[vid].setHeading(-Math.PI / 180 * (data2.heading));
+                     if(this.models[vid] instanceof dl.Model)
+                    {
+                        this.models[vid].setHeading(-Math.PI / 180 * (data2.heading));
+                    }else{
+                        this.models[vid].rotation.set( this.pitch,this.yaw,(-Math.PI / 180) * data2.heading);
+                    }
                     //vid与设定的主车vid相等则做相机跟随操作
                     if(this.mainCarVID == vid)
                     {
@@ -946,9 +911,16 @@ export default {
                             (d)=>{
                                 // console.log("onUpdate");
                                 // console.log(d);
-                                this.models[vid].position.x = this.carBusinessData[vid]['lastUtmPosition'].x;
-                                this.models[vid].position.y = this.carBusinessData[vid]['lastUtmPosition'].y;
-                                this.models[vid].setHeading(-Math.PI / 180 * (this.carBusinessData[vid]['lastUtmPosition'].rotation));
+                                if(this.models[vid] instanceof dl.Model)
+                                {
+                                    this.models[vid].position.x = this.carBusinessData[vid]['lastUtmPosition'].x;
+                                    this.models[vid].position.y = this.carBusinessData[vid]['lastUtmPosition'].y;
+                                    this.models[vid].setHeading(-Math.PI / 180 * (this.carBusinessData[vid]['lastUtmPosition'].rotation));
+                                }else{
+                                    this.models[vid].position.set(this.carBusinessData[vid]['lastUtmPosition'].x,this.carBusinessData[vid]['lastUtmPosition'].y, this.defualtZ );
+                                    // this.changeModelColor(data2,this.models[vid]);
+                                    this.models[vid].rotation.set( this.pitch,this.yaw,(-Math.PI / 180) * this.carBusinessData[vid]['lastUtmPosition'].rotation);
+                                }
                             
                             //    this.infoLabels[vid].position.x = this.carBusinessData[vid]['lastUtmPosition'].x;
                             //    this.infoLabels[vid].position.y = this.carBusinessData[vid]['lastUtmPosition'].y;
@@ -982,10 +954,17 @@ export default {
                                         (d)=>{
                                             // console.log("onUpdate");
                                             // console.log(d);
-                                            this.models[vid].position.x = this.carBusinessData[vid]['lastUtmPosition'].x;
-                                            this.models[vid].position.y = this.carBusinessData[vid]['lastUtmPosition'].y;
-                                            this.models[vid].setHeading(-Math.PI / 180 * (this.carBusinessData[vid]['lastUtmPosition'].rotation));
-                                            
+                                            if(this.models[vid] instanceof dl.Model)
+                                            {
+                                                this.models[vid].position.x = this.carBusinessData[vid]['lastUtmPosition'].x;
+                                                this.models[vid].position.y = this.carBusinessData[vid]['lastUtmPosition'].y;
+                                                this.models[vid].setHeading(-Math.PI / 180 * (this.carBusinessData[vid]['lastUtmPosition'].rotation));
+                                            }else{
+                                                this.models[vid].position.set(this.carBusinessData[vid]['lastUtmPosition'].x,this.carBusinessData[vid]['lastUtmPosition'].y, this.defualtZ );
+                                                // this.changeModelColor(data2,this.models[vid]);
+                                                this.models[vid].rotation.set( this.pitch,this.yaw,(-Math.PI / 180) * this.carBusinessData[vid]['lastUtmPosition'].rotation);
+                                            }
+
                                             
                                             // this.infoLabels["gan"][vid].position.set(this.carBusinessData[vid]['lastUtmPosition'].x,this.carBusinessData[vid]['lastUtmPosition'].y,this.defualtZ+4);
                                             // this.infoLabels["label"][vid].setPositon([this.carBusinessData[vid]['lastUtmPosition'].x,this.carBusinessData[vid]['lastUtmPosition'].y,this.defualtZ+8]);
@@ -1027,7 +1006,6 @@ export default {
                 }
 
             }
-           
         },
         pageResize(){
             const borwserHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
