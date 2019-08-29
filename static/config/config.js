cfb65b10ef065c59d71b5c26925216a249c5bd68
shaoxiaoli <shaoxiaoli@tusvn.com>
2019-08-29 19:11:12 +0800

    路段的车辆代码优化

--- static/config/config.js
+++ static/config/config.js
@@ -1,13 +1,13 @@
 window.config = {
 
     // http://172.17.1.13:9092/icvShadowApp/ws.html
-  /*  //望京地址  内网
-    url: 'http://172.17.1.13:9093/monPlatApp/', //监控平台
-    websocketUrl:'ws://172.17.1.13:49982/mon',  //监控
-    socketUrl:'ws://172.17.1.13:49999/ws',  //影子系统
-    dlUrl: 'http://10.0.1.22:8085/', //迪路
-    dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
-    roadUrl:'http://172.17.1.13:28080/rCUDataApp/', //临时演示地址*/
+    //望京地址  内网
+    // url: 'http://172.17.1.13:9093/monPlatApp/', //监控平台
+    // websocketUrl:'ws://172.17.1.13:49982/mon',  //监控
+    // socketUrl:'ws://172.17.1.13:49999/ws',  //影子系统
+    // dlUrl: 'http://10.0.1.22:8085/', //迪路
+    // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
+    // roadUrl:'http://172.17.1.13:28080/rCUDataApp/', //临时演示地址
 
     //外网
     url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
