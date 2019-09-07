8535c506d47025825db3814919e5e9d37987ff9b
shilu <shilu@tusvn.comm>
2019-09-07 20:22:42 +0800

    配置config

--- static/config/config.js
+++ static/config/config.js
@@ -1,22 +1,12 @@
 window.config = {
+    //长沙
+       url: 'http://111.23.140.59:9093/monPlatApp/', //监控平台
+       operateUrl: 'http://111.23.140.59:9090/operateApp/', //运营平台
+       websocketUrl:'ws://111.23.140.59:9982/mon',  //监控
+       socketUrl:'ws://111.23.140.59:9999/ws',  //影子系统
+       dlUrl: 'http://111.23.140.59:8085/', //迪路
+       dlWmsUrl: 'http://111.23.140.59:8080/', //迪路
 
-    // http://172.17.1.16:9092/icvShadowApp/ws.html
-  /*  //望京地址  内网
-    url: 'http://172.17.1.16:9093/monPlatApp/', //监控平台
-    operateUrl: 'http://172.17.1.16:9090/operateApp/',	//运营平台
-    websocketUrl:'ws://172.17.1.16:49982/mon',  //监控
-    socketUrl:'ws://172.17.1.16:49999/ws',  //影子系统
-    dlUrl: 'http://10.0.1.22:8085/', //迪路
-    dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
-    */
-
-    //外网
-    url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
-    operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
-    websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
-    socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
-    dlUrl: 'http://113.208.118.62:8085/', //迪路
-    dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
     version: 1.0,       // 版本号
 }
 
@@ -34,20 +24,6 @@ window.defaultMapOption = {
     rotateEnable: true,
     mapStyle: "amap://styles/3312a5b0f7d3e828edc4b2f523ba76d8"
 }
-
-// 调用
-// this.distanceMap = new AMap.Map('map-container', window.defaultMapOption);
-// let _option = Object.assign(
-// 	{},
-// 	window.defaultMapOption,
-// 	{
-// 		mapStyle: window.mapOption.mapStyleEmpty
-// 	}
-// );
-// this.distanceMap = new AMap.Map('map-container', _option);
-
-
-
 //路网配置参数
 window.dlWmsOption = {
     'LAYERS_gjlk': 'shanghai_qcc:dl_shcsq_wgs84_gjlk',
@@ -65,16 +41,3 @@ window.dlWmsDefaultOption = {
     tileSize: 256,
     params:{}
 }
-//	调用
-// let _optionWms = Object.assign(
-// 	{},
-// 	window.dlWmsDefaultOption,
-// 	{
-// 		params:{'LAYERS': window.dlWmsOption.LAYERS_centerline,'STYLES': window.dlWmsOption.STYLES, 'VERSION': window.dlWmsOption.VERSION}
-// 	}
-// );
-// this.wms = new AMap.TileLayer.WMS(_optionWms);
-// this.distanceMap.add(this.wms);
-
-
-// ws://120.133.21.14:29998/ws
