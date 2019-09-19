b95d792fa454d35e4178cce699b934805e8709e9
邱丽娜 <qiulina@tusvn.com>
2019-09-19 11:03:07 +0800

    地图提取

--- static/config/config.js
+++ static/config/config.js
@@ -10,22 +10,22 @@ window.config = {
     //http://172.17.1.16:9092/icvShadowApp/ws.html  不要给我删掉了！！！
 
     //望京 外网
-    url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
-    operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
-    websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
-    socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
-    dlUrl: 'http://113.208.118.62:8085/', //迪路
-    dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
+    // url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
+    // operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
+    // websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
+    // socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
+    // dlUrl: 'http://113.208.118.62:8085/', //迪路
+    // dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
 
     //上海正式环境  外网
-    // url: 'http://116.236.72.206:49093/monPlatApp/', //监控平台
-    // operateUrl: 'http://116.236.72.204:49090/operateApp/', //运营平台
-    // websocketUrl:'ws://116.236.72.206:49982/mon',  //监控
-    // socketUrl:'ws://116.236.72.205:49999/ws',  //影子系统
-    // dlUrl: 'http://116.236.72.204:48085/', //迪路
-    // dlWmsUrl: 'http://116.236.72.204:48080/', //迪路
-    // //
-    //  version: 1.0,       // 版本号
+    url: 'http://116.236.72.206:49093/monPlatApp/', //监控平台
+    operateUrl: 'http://116.236.72.204:49090/operateApp/', //运营平台
+    websocketUrl:'ws://116.236.72.206:49982/mon',  //监控
+    socketUrl:'ws://116.236.72.205:49999/ws',  //影子系统
+    dlUrl: 'http://116.236.72.204:48085/', //迪路
+    dlWmsUrl: 'http://116.236.72.204:48080/', //迪路
+    //
+     version: 1.0,       // 版本号
 }
 
 //地图额外配置项添加
@@ -41,6 +41,10 @@ window.defaultMapOption = {
     resizeEnable: true, //是否监控地图容器尺寸变化
     rotateEnable: true,
     mapStyle: "amap://styles/3312a5b0f7d3e828edc4b2f523ba76d8"
+}
+//天气
+window.weatherCenter={
+    center:[121.17265957261286,31.284096076877844]
 }
 
 // 调用
@@ -72,6 +76,16 @@ window.dlWmsDefaultOption = {
     blend: false,
     tileSize: 256,
     params:{}
+}
+
+window.mapParam={
+    minX:325295.155400,
+    minY:3461941.703700,
+    minZ:50,
+    maxX:326681.125700,
+    maxY:3462723.022400,
+    maxZ:80,
+    background:"black"
 }
 //	调用
 // let _optionWms = Object.assign(
