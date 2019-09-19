bd75564aadbc1249298a1d66c816a58e29a5ca60
shilu <shilu@tusvn.comm>
2019-09-19 11:38:01 +0800

    优化地址

--- static/config/config.js
+++ static/config/config.js
@@ -6,32 +6,6 @@ window.config = {
     socketUrl:'ws://218.76.44.22:9999/ws',  //影子系统
     dlUrl: 'http://218.76.44.22:8085/', //迪路
     dlWmsUrl: 'http://218.76.44.22:48080/', //迪路
-
-    //望京 内网
-    // url: 'http://172.17.1.16:9093/monPlatApp/', //监控平台
-    // operateUrl: 'http://172.17.1.16:9090/operateApp/',	//运营平台
-    // websocketUrl:'ws://172.17.1.16:49982/mon',  //监控
-    // socketUrl:'ws://172.17.1.16:49999/ws',  //影子系统
-    // dlUrl: 'http://10.0.1.22:8085/', //迪路
-    // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
-    //http://172.17.1.16:9092/icvShadowApp/ws.html  不要给我删掉了！！！
-
-    //望京 外网
-    // url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
-    // operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
-    // websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
-    // socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
-    // dlUrl: 'http://113.208.118.62:8085/', //迪路
-    // dlWmsUrl: 'http://113.208.118.62:8080/', //迪路
-
-    //上海正式环境  外网
-    // url: 'http://116.236.72.206:49093/monPlatApp/', //监控平台
-    // operateUrl: 'http://116.236.72.204:49090/operateApp/', //运营平台
-    // websocketUrl:'ws://116.236.72.206:49982/mon',  //监控
-    // socketUrl:'ws://116.236.72.205:49999/ws',  //影子系统
-    // dlUrl: 'http://116.236.72.204:48085/', //迪路
-    // dlWmsUrl: 'http://116.236.72.204:48080/', //迪路
-    // version: 1.0,       // 版本号
 }
 
 //地图额外配置项添加
