79a10c54e274fe1998b4abd7c0de3398f69816fd
邱丽娜 <qiulina@tusvn.com>
2019-11-06 14:30:58 +0800

    '地图修改'

---
+++ src/utils/GIS3D.js
@@ -0,0 +1,389 @@
+class GIS3D {
+  constructor() {
+    this.cesiumContainer = null;
+    /*this.maxWGS84 = [112.95003033070373, 28.326432159727982];
+    this.minWGS84 = [112.94760914128275, 28.325093927226323];*/
+    this.cesium = { viewer: null };
+    this.models={};
+  }
+  initload(id,isFXAA) {
+ 
+    this.cesiumContainer = document.getElementById(id);
+    this.initCesium(); // Initialize Cesium renderer  
+    if(isFXAA)
+    {
+      this.cesium.viewer.scene.postProcessStages.fxaa.enabled = isFXAA;
+    }
+    // PerceptionCars.initPerceptionCount(this);
+    // PerceptionCars.processPerceptionMesage(this);
+  }
+  initCesium() {
+    this.cesium.viewer = new Cesium.Viewer(this.cesiumContainer, {
+      projectionPicker: true, animation: false,  //动画控制不显示     
+      timeline: false
+      ,    //时间线不显示
+      imageryProvider: new Cesium.SingleTileImageryProvider({
+        url: 'static/images/2.png'//透明图片
+      }), 
+      geocoder: false,
+      homeButton: false,
+      sceneModePicker: false,
+      baseLayerPicker: false,
+      navigationHelpButton: false,
+      fullscreenButton: false,
+      vrButton: false
+
+    });
+
+    this.cesium.viewer.scene.screenSpaceCameraController.zoomEventTypes = [Cesium.CameraEventType.WHEEL, Cesium.CameraEventType.PINCH];
+    this.cesium.viewer.scene.screenSpaceCameraController.tiltEventTypes = [Cesium.CameraEventType.PINCH, Cesium.CameraEventType.RIGHT_DRAG];
+
+    // this.cesium.viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({
+    //               url: "http://webst02.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8",
+    //   //            layer: "tdtAnnoLayer",
+    //   //            style: "default",
+    //   //            format: "image/jpeg",
+    //   //            tileMatrixSetID: "GoogleMapsCompatible"，
+    //   maximumLevel: 24
+    //           })); 
+    //增加谷歌影像底图
+    // this.cesium.viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({
+    //   url: 'http://www.google.cn/maps/vt?lyrs=s&x={x}&y={y}&z={z}',
+    //   tilingScheme: new Cesium.WebMercatorTilingScheme()
+    // })
+    // );
+
+
+    // this.cesium.viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({
+    //   url: 'http://www.google.cn/maps/vt?lyrs=s&x={x}&y={y}&z={z}',
+    //   tilingScheme: new Cesium.WebMercatorTilingScheme()
+    // })
+    // );
+
+ 
+    // // //上海数据
+    this.cesium.viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/data/3.geojson', {
+      fill: Cesium.Color.ALICEBLUE, 
+      markerSymbol: '?'
+    }));
+    this.cesium.viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/data/2.geojson', {
+      stroke: Cesium.Color.fromCssColorString('#894b2b'),// Cesium.Color.ORANGE,
+      strokeWidth: 2,
+      markerSymbol: '?'
+    }));
+    this.cesium.viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/data/1.geojson', {
+      stroke: Cesium.Color.ALICEBLUE,
+      strokeWidth: 2,
+      markerSymbol: '?'
+    }));
+   
+    //   this.cesium.viewer.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+    //   url: 'http://47.94.231.163:8088/geoserver/gwc/service/wmts/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0' +
+    //     '&LAYER=yz:YZ_hdmap_base&STYLE=&FORMAT=image/png&TILEMATRIXSET=EPSG:900913&TileMatrix=EPSG:900913:{TileMatrix}&TileCol={TileCol}&TileRow={TileRow}', //服务地址
+    //   maximumLevel: 24
+    // }));
+
+
+
+
+    // //增加业务图层
+    // this.cesium.viewer.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+    //   url: 'http://10.0.1.22:8080/geoserver/gwc/service/wmts/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0' +
+    //     '&LAYER=shanghai_qcc:dl_shcsq_wgs84_zc_0708&STYLE=&FORMAT=image/png&TILEMATRIXSET=EPSG:900913&TileMatrix=EPSG:900913:{TileMatrix}&TileCol={TileCol}&TileRow={TileRow}', //服务地址
+    //   maximumLevel: 20
+    // }));
+
+    //长沙
+    this.cesium.viewer.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+      url: 'http://218.76.44.22:48080/geoserver/gwc/service/wmts/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0' +
+        '&LAYER=shanghai_qcc:dl_shcsq_wgs84_zc_0709&STYLE=&FORMAT=image/png&TILEMATRIXSET=EPSG:900913&TileMatrix=EPSG:900913:{TileMatrix}&TileCol={TileCol}&TileRow={TileRow}', //服务地址
+      maximumLevel: 20
+    }));
+    //去除版权信息
+    this.cesium.viewer._cesiumWidget._creditContainer.style.display = "none";
+
+
+    //////////////世界坐标转换为经纬度
+
+    // var ellipsoid=this.cesium.viewer.scene.globe.ellipsoid;
+    // var cartesian3=new Cesium.Cartesian3( -2190690.3979744054,5174092.211871381,3008286.1630493593);
+    // // var cartesian3=new Cesium.Cartesian3(this.cesium.viewer.camera.position["x"],this.cesium.viewer.camera.position["y"],this.cesium.viewer.camera.position["z"]);
+    // var cartographic=ellipsoid.cartesianToCartographic(cartesian3);
+    // var lat=Cesium.Math.toDegrees(cartographic.latitude);
+    // var lng=Cesium.Math.toDegrees(cartographic.longitude);
+    // var alt=cartographic.height; 
+    /////////////////
+
+   /* var heading = Cesium.Math.toRadians(70);
+    var pitch = -0.2369132859032279;
+    var roll = 0.0029627735803421373;
+    var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+    this.cesium.viewer.camera.flyTo({
+      destination: Cesium.Cartesian3.fromDegrees(this.minWGS84[0], this.minWGS84[1], 39.142101722743725),
+      orientation: hpr
+    });*/
+    // this.initStreetLamp();
+    this.initModel_pole();
+    
+    this.add3DInfoLabel("aaa","慢行",112.95003033070373, 28.326432159727982,10)
+
+    // this.remove3DInforLabel("aaa")
+
+
+    let v = this.cesium.viewer;
+    this.cesium.viewer.scene.camera.moveEnd.addEventListener(function () { 
+      if (v.dataSources.length == 0) return;
+      var currentMagnitude = v.camera.getMagnitude();
+      // console.log('currentMagnitude - ' + currentMagnitude);
+      if (currentMagnitude <= 6373393.152436117) { 
+        v.dataSources.get(0).show = true;
+        if (v.dataSources.length == 2) {
+          v.dataSources.get(1).show = true;
+        } 
+        if (v.imageryLayers.length == 2) {
+          v.imageryLayers.get(1).show = false;
+        } 
+        // v.scene.postProcessStages.fxaa.enabled = true;
+      }
+      else {
+        if (v.dataSources.get(0).show) {
+          v.dataSources.get(0).show = false;
+          if (v.dataSources.length == 2) {
+            v.dataSources.get(1).show = false;
+          }; 
+          if (v.imageryLayers.length == 2) {
+            v.imageryLayers.get(1).show = true;
+          } 
+        }
+        // v.scene.postProcessStages.fxaa.enabled = false;
+      }
+    })
+
+
+    ////////////////////
+
+
+    this.cesium.viewer.scene.screenSpaceCameraController.minimumZoomDistance =6; //距离地形的距离？这个值可以多测试几个值，，我这不太好描述
+    //  this.cesium.viewer.clock.onTick.addEventListener(function () {
+    //    if(v.camera.pitch > 0){
+    //      v.scene.screenSpaceCameraController.enableTilt = false;
+    //     }
+    //  })
+    //  var startMousePosition;
+    //  var mousePosition;
+    //  var handler = new Cesium.ScreenSpaceEventHandler(canvas);
+    //  handler.setInputAction(function(movement) {
+    //      mousePosition = startMousePosition = Cesium.Cartesian3.clone(movement.position);
+    //      handler.setInputAction(function(movement) {
+    //          mousePosition = movement.endPosition;
+    //          var y = mousePosition.y - startMousePosition.y;
+    //          if(y>0){
+    //              scene.screenSpaceCameraController.enableTilt = true;
+    //          }
+    //      }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    //  }, Cesium.ScreenSpaceEventType.MIDDLE_DOWN); 
+    ///////////////////////
+  }
+
+  
+
+  /**
+   * 加载感知杆
+   */
+  initModel_pole()//初始化杆
+  {
+    var item = sessionStorage.getItem("sideList");
+    var itemSide = JSON.parse(item);
+    // console.log(item)
+    if (itemSide != null && itemSide.length > 0) {
+      var entity = null;
+      //合并写法
+      var instances = [];
+      var labels = this.cesium.viewer.scene.primitives.add(new Cesium.LabelCollection());
+      for (var i = 0; i < itemSide.length; i++) {
+        labels.add({
+          fillColor  :Cesium.Color.BLACK ,
+          backgroundColor: Cesium.Color.fromCssColorString('#fff'),
+          position: Cesium.Cartesian3.fromDegrees(itemSide[i].longitude, itemSide[i].latitude, 5),
+          text: itemSide[i].devName,
+          font: '14px sans-serif',
+          showBackground: true,
+          horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+          pixelOffset: new Cesium.Cartesian2(0.0, 0),
+          // pixelOffsetScaleByDistance: new Cesium.NearFarScalar(1.5e2, 3.0, 1.5e7, 0.0),
+          scaleByDistance: new Cesium.NearFarScalar(1000, 1, 8000, 0)
+        });
+        var position = Cesium.Cartesian3.fromDegrees(itemSide[i].longitude, itemSide[i].latitude, 0);
+        //  
+        var heading = Cesium.Math.toRadians(itemSide[i].heading + 90);
+        var pitch = Cesium.Math.toRadians(0);
+        var roll = 0;
+        var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+        var modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(position, hpr);
+        instances.push({
+          modelMatrix: modelMatrix
+        });
+      }
+      this.cesium.viewer.scene.primitives.add(new Cesium.ModelInstanceCollection({
+        url: './static/model/poleWith2Camera.glb',
+        instances: instances
+      }));
+    }
+  }
+   computeCircle(radius) {
+    var positions = [];
+    for (var i = 0; i < 360; i++) {
+        var radians = Cesium.Math.toRadians(i);
+        positions.push(new Cesium.Cartesian3(radius * Math.cos(radians), radius * Math.sin(radians),10));
+    }
+    return positions;
+}
+remove3DInforLabel(name) { 
+  let label = this.models[name];
+  if (label != null) {
+    this.cesium.viewer.entities.remove(label);
+      delete this.models[name];
+  }
+}
+  add3DInfoLabel(name, text, x, y, z) 
+    { 
+     let positions = []; 
+      positions.push(Cesium.Cartesian3.fromDegrees(x,y,0));
+      positions.push(Cesium.Cartesian3.fromDegrees(x,y,20)); 
+     let lableModel= this.cesium.viewer.entities.add({
+        id: name,
+        position: Cesium.Cartesian3.fromDegrees(x,y,20),
+        // point: {
+        //   color: Cesium.Color.RED,    //点位颜色
+        //   pixelSize: 10          //像素点大小
+        // },  
+        polyline : {
+            positions : positions,
+            width :3,
+            material:Cesium.Color.fromCssColorString('#ab6503')
+            // material : new Cesium.PolylineGlowMaterialProperty({
+            //     color : Cesium.Color.fromCssColorString('#ab6503')
+            //     // ,//Cesium.Color.DEEPSKYBLUE, 0xab6503
+            //     // glowPower : 0.25
+            // })
+        },
+        label: {
+          text:  text,
+        
+          // fillColor  :Cesium.Color.BLACK ,
+          backgroundColor: Cesium.Color.fromCssColorString('#894b2b'),
+          font: '30px sans-serif',
+          showBackground: true,
+          horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+          pixelOffset: new Cesium.Cartesian2(0.0, 0),
+          // pixelOffsetScaleByDistance: new Cesium.NearFarScalar(1.5e2, 3.0, 1.5e7, 0.5),
+          scaleByDistance : new Cesium.NearFarScalar(2000, 1, 8000, 0)//new Cesium.NearFarScalar(1.5e2, 1.5, 1.0e5, 0.0)
+        }
+      }); 
+
+      this.models[name] =lableModel;
+    }
+    getExtent() {
+      // this.cesium.viewer
+    }
+     /**
+             *获取相机参数
+             */
+            getCamera() {
+              let obj = {
+                  x: this.cesium.viewer.camera.position.x,
+                  y: this.cesium.viewer.camera.position.y,
+                  z:this.cesium.viewer.camera.position.z,
+                  radius: this.cesium.viewer.camera.heading,
+                  pitch: this.cesium.viewer.camera.pitch,
+                  yaw: this.cesium.viewer.camera.roll
+              };
+              return obj;
+          }
+       addModel(name, url, x, y, z) {
+        var position = Cesium.Cartesian3.fromDegrees(x,y, z); 
+        var heading = Cesium.Math.toRadians(0);
+        var pitch = Cesium.Math.toRadians(0);
+        var roll = 0;
+        var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+        var modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(position, hpr);
+        var entity =Cesium.Model.fromGltf({
+          id: name,
+          modelMatrix: modelMatrix,
+          url: './static/model/car.glb',
+          minimumPixelSize: 80,
+          show:false,
+          maximumScale:5
+        //   ,
+        //   scale : 3.0     //放大倍数
+          // debugWireframe:true
+        })
+        //e.cesium.viewer.entities.add(entity);
+        this.cesium.viewer.scene.primitives.add(entity);
+          }
+    updateCameraPosition(x, y, z, radius, pitch, yaw)
+    {
+      var heading = Cesium.Math.toRadians(radius);
+      // var pitch = -0.2369132859032279;
+      // var roll = 0.0029627735803421373;
+
+        var hpr = new Cesium.HeadingPitchRoll(heading, pitch, yaw);
+        this.cesium.viewer.camera.flyTo({
+            destination: Cesium.Cartesian3.fromDegrees(x, y, 39.142101722743725),
+            orientation: hpr
+        });
+
+    }
+    updatePosition(minx,miny,maxx,maxy){
+        var rectangle = new Cesium.Rectangle.fromDegrees(minx,miny,maxx,maxy);
+        this.cesium.viewer.camera.flyTo({
+            destination: rectangle
+        });
+    }
+  /**
+   * 加载灯杆
+   */
+  initStreetLamp() {
+    debugger
+    //添加路灯杆和信息牌
+    let itemSide = [[112.95003033070373, 28.326432159727982, 0]]
+
+    // console.log(item)
+    if (itemSide != null && itemSide.length > 0) {
+      var entity = null;
+      //合并写法
+      var instances = [];
+      var labels = this.cesium.viewer.scene.primitives.add(new Cesium.LabelCollection());
+      for (var i = 0; i < itemSide.length; i++) {
+        labels.add({
+          fillColor  :Cesium.Color.BLACK ,
+          backgroundColor: Cesium.Color.fromCssColorString('#fff'),
+          position: Cesium.Cartesian3.fromDegrees(itemSide[i][0], itemSide[i][1], 5),
+          text: "sdfsd",
+          font: '20px sans-serif',
+          showBackground: true,
+          horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+          pixelOffset: new Cesium.Cartesian2(0.0, 0),
+          // pixelOffsetScaleByDistance: new Cesium.NearFarScalar(1.5e2, 3.0, 1.5e7, 0.0),
+          scaleByDistance: new Cesium.NearFarScalar(1000, 1, 8000, 0)
+        }); 
+        var position = Cesium.Cartesian3.fromDegrees(itemSide[i][0], itemSide[i][1], 0);
+        //  
+        var heading = Cesium.Math.toRadians(30);
+        var pitch = Cesium.Math.toRadians(0);
+        var roll = 0;
+        var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+        var modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(position, hpr);
+        instances.push({
+          modelMatrix: modelMatrix
+        });
+      }
+      this.cesium.viewer.scene.primitives.add(new Cesium.ModelInstanceCollection({
+        url: './static/model/street_lamp_two1.glb',
+        instances: instances
+      }));
+    }   
+  }
+ 
+}
+export default GIS3D
---
+++ static/config/configshanghai.js
@@ -0,0 +1,111 @@
+window.config = {
+    
+    //望京 内网
+    // staticUrl: "http://172.17.1.16:9090/", //静态文件路径
+    // url: 'http://172.17.1.16:9093/monPlatApp/', //监控平台
+    // operateUrl: 'http://172.17.1.16:9090/operateApp/',	//运营平台
+    // websocketUrl:'ws://172.17.1.16:49982/mon',  //监控
+    // socketUrl:'ws://172.17.1.16:49999/ws',  //影子系统
+    // dlUrl: 'http://10.0.1.22:8085/', //迪路
+    // dlWmsUrl: 'http://10.0.1.22:8080/', //迪路
+    //http://172.17.1.16:9092/icvShadowApp/ws.html  不要给我删掉了！！！
+
+    //望京 外网
+    // staticUrl: "http://120.133.21.14:9090/", //静态文件路径
+    // url: 'http://120.133.21.14:9093/monPlatApp/', //监控平台
+    // operateUrl: 'http://120.133.21.14:9090/operateApp/',	//运营平台
+    // websocketUrl:'ws://120.133.21.14:49982/mon',  //监控
+    // socketUrl:'ws://120.133.21.14:49999/ws',  //影子系统
+    // dlUrl: 'http://117.114.144.227:8085/', //迪路
+    // dlWmsUrl: 'http://117.114.144.227:8080/', //迪路
+
+    //上海正式环境  外网
+    staticUrl: "http://116.236.72.204:49090/", //静态文件路径
+    url: 'http://116.236.72.206:49093/monPlatApp/', //监控平台
+    operateUrl: 'http://116.236.72.204:49090/operateApp/', //运营平台
+    websocketUrl:'ws://116.236.72.206:49982/mon',  //监控
+    socketUrl:'ws://116.236.72.205:49999/ws',  //影子系统
+    dlUrl: 'http://116.236.72.204:48085/', //迪路
+    dlWmsUrl: 'http://116.236.72.204:48080/', //迪路
+
+     version: 1.0,       // 版本号
+}
+
+//地图额外配置项添加
+window.mapOption = {
+     center: [121.17265957261286,31.284096076877844],	//上海-高德地图坐标点  感知右下角
+    // center: [113.07876,28.255618],	//长沙-高德地图坐标点
+    mapStyleEmpty: "amap://styles/bc5a63d154ee0a5221a1ee7197607a00", // 纯灰色背景地图
+};
+//单车
+window.defaultMapOption = {
+    center: window.mapOption.center, //上海
+    zoom: 11,		// 默认：比例尺显示100m
+    resizeEnable: true, //是否监控地图容器尺寸变化
+    rotateEnable: true,
+    mapStyle: "amap://styles/3312a5b0f7d3e828edc4b2f523ba76d8"
+}
+
+//路网配置参数
+window.dlWmsOption = {
+    'LAYERS_gjlk': 'shanghai_qcc:dl_shcsq_wgs84_gjlk',
+    'LAYERS_withoutz': 'shanghai_qcc:dl_shcsq_wgs84_rc_withoutz',
+    'VERSION':'1.1.0',
+    'GD_ROAD_CENTERLINE':'gd_road_centerline',
+
+    'LAYERS_centerline': 'shanghai_qcc:dl_shcsq_wgs84_lane_centerline',
+    'STYLES':'shanghai_qcc:dl_shcsq_wgs84_road_centerline_car_statistics',
+
+}
+window.dlWmsDefaultOption = {
+    url:window.config.dlWmsUrl+'geoserver/shanghai_qcc/wms',
+    blend: false,
+    tileSize: 256,
+    params:{}
+}
+window.circleParam = {
+    redius:1000.0,
+    center:[325535.061,3462402.816,13.618],
+    color:"#ffffff",
+    outline:false,
+    outlineColor:"#ff0000",
+    opacity:0.1,
+    rotate:[0,0,0]
+};
+
+window.mapParam={
+    minX:325295.155400,
+    minY:3461941.703700,
+    minZ:50,
+    maxX:326681.125700,
+    maxY:3462723.022400,
+    maxZ:80,
+    background:"black"
+}
+
+window.defaultMapParam = {
+    x:326299.8136019115,
+    y:3462328.443327571,
+    z:34.16186920538662,
+    radius:31.40011218302981,
+    pitch:-0.1440529053876541,
+    yaw:-2.7068034133160297
+}
+
+window.defaultSingleParam = {
+    x:326181.72659014474,
+    y:3462354.6747002415,
+    z:737.3642832288795,
+    radius:741.5052736914325,
+    pitch:-1.5707963267948966,
+    yaw:-0.05266622778143515
+}
+
+window.defaultRoadParam={
+    z:217.16763677929166,
+    radius:0,
+    pitch:-1.5707963267948966,
+    yaw:-0.16236538804906267
+}
+
+window.currentExtent=[[121.431,31.113],[121.063,31.113],[121.063,31.371],[121.431,31.371]];
