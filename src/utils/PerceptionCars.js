24898c77471488ce0e1db08d2e2bcdfaeec6b2d8
chiyanchao@tusvn.com <chiyanchao@tusvn.com>
2019-11-07 13:56:18 +0800

    实时模型数据更新

--- src/utils/PerceptionCars.js
+++ src/utils/PerceptionCars.js
@@ -16,209 +16,205 @@ class PerceptionCars {
     //接受数据
     addPerceptionData(data) {
         //  console.log(11);
-        this.cachePerceptionQueue.push(data);
-
+        // this.cachePerceptionQueue.push(data);
+        // this.cachePerceptionQueue= new Array();
+        // this.cachePerceptionQueue.push(data);
+        this.processPerceptionMesage(data);
     }
     //绘制感知车
-    processPerceptionMesage() {
+    processPerceptionMesage(data) {
 
-        let _this=this;
+        let _this = this;
 
-        _this.processPerceptionDataIntervalId = setInterval(() => {
-            if(_this.deviceModels==undefined)return;
-            // let l=Object.getOwnPropertyNames(_this.deviceModels.cars).length;
-            // console.log(l)
-            // requestAnimationFrame(PerceptionCars.processPerceptionMesage);
-            if (_this.cachePerceptionQueue == null)
-                return;
-            if (_this.cachePerceptionQueue.length == 0)
-                return;
-            _this.lastPerceptionMessage = _this.cachePerceptionQueue.shift();
-            let data = null;
-            if (_this.lastPerceptionMessage == null) {
-                return;
-            } else {
-                data = _this.lastPerceptionMessage;
+        // _this.processPerceptionDataIntervalId = setInterval(() => {
+        if (_this.deviceModels == undefined) return;
+        // let l=Object.getOwnPropertyNames(_this.deviceModels.cars).length;
+        // console.log(l)
+        // requestAnimationFrame(PerceptionCars.processPerceptionMesage);
+        // if (_this.cachePerceptionQueue == null)
+        //   return;
+        // if (_this.cachePerceptionQueue.length == 0)
+        //   return;
+        //   _this.lastPerceptionMessage = _this.cachePerceptionQueue.shift();
+        // let data = null;
+        // if (_this.lastPerceptionMessage == null) {
+        //   return;
+        // } else {
+        //   data = _this.lastPerceptionMessage;
+        // }
+        if (data == null) return;
+        var data2 = JSON.parse(data.data);
+        // var data2 = JSON.parse(data);
+        let fusionList = data2.result.vehDataDTO;
+        // for(var i=0;i<1000;i++)
+        // {
+
+        //     var xy=fusionList[0];
+        //    xy.latitude=fusionList[0].latitude+0.000001*i;
+        //     xy.longitude=fusionList[0].longitude+0.000001*i;
+        //     fusionList.push(xy)
+        // }
+
+
+        let count = 0;
+        //复位感知车
+        if (fusionList.length > 0) {
+            var primitives = _this.viewer.scene.primitives;
+            for (var i = 0; i < primitives.length; i++) {
+                var primitive = primitives.get(i);
+
+                let isTrue = false;
+                for (var kk = 0; kk < fusionList.length; kk++) {
+                    if (primitive instanceof Cesium.Model && (primitive.id != fusionList[kk].vehicleId + "car")) {
+                        isTrue = true;
+                        continue;
+                    }
+                }
+                if (isTrue) {
+                    primitive.show = false;
+                    count++;
+                }
             }
-            var data2 = JSON.parse(data.data);
-            // var data2 = JSON.parse(data);
-            let fusionList = data2.result.vehDataDTO;
-            // for(var i=0;i<1000;i++)
-            // {
+            if ((count - fusionList.length) >= 20) {
+                this.removeModelPrimitives();
+            }
+            console.log(fusionList.length + "show" + count)
+            // var primitives = this.viewer.scene.primitives;
+            // var length = primitives.length;
 
-            //     var xy=fusionList[0];
-            //    xy.latitude=fusionList[0].latitude+0.000001*i;
-            //     xy.longitude=fusionList[0].longitude+0.000001*i;
-            //     fusionList.push(xy)
+            // for (var k = 0; k < length; ++k) {
+            //   var p = primitives.get(k);
+            //   if (p.id == undefined) continue;
+            //   if (p.id.search("model") != -1) {
+            //     p.show = false;
+            //   }
             // }
 
+            // var entities = this.viewer.entities;//  .getById(i + "label");
+            // var enlength = entities.values.length;
+            // for (var k = 0; k < enlength; ++k) {
+            //   var lbl = entities.values[k];
+            //   if (lbl.id == undefined) continue;
+            //   if (lbl.id.search("label") != -1) {
+            //     lbl.show = false;
+            //   }
+            // }
+            // for (var k = 0; k < length; ++k) {
+            //   var p = primitives.get(k);
+            //   if (p.id == undefined) continue;
+            //   if (p.id.search("person") != -1) {
+            //     p.show = false;
+            //   }
+            // }
 
-            let count=0;
-            //复位感知车
-            if (fusionList.length > 0) {
-                var primitives = _this.viewer.scene.primitives;
-                for (var i = 0; i < primitives.length; i++) {
-                    var primitive = primitives.get(i);
+            // for (let p = 0; p < this.deviceModels.cars.length; p++) {
+            //   let carmodel = this.viewer.entities.getById(p + "model");
+            //   var position = Cesium.Cartesian3.fromDegrees(-1000, -1000, 0.0);
+            //   carmodel.position = position;
+            // }
 
-                    let isTrue=false;
-                    for(var kk=0;kk<fusionList.length;kk++)
-                    {
-                        if(primitive instanceof Cesium.Model &&(primitive.id!=fusionList[kk].vehicleId+"car"))
-                        {
-                            isTrue=true;
-                            continue;
-                        }
-                    }
-                    if(isTrue)
-                    {
-                        primitive.show=false;
-                        count++;
-                        // _this.viewer.scene.primitives.remove(primitive);
-                    }
+            // for (let q = 0; q < this.deviceModels.persons.length; q++) {
+            //     let person = this.deviceModels.persons[q];
+            //     person.position.x = 0;
+            //     person.position.y = 0;
+            //     person.position.z = 0;
+            // }
+            // for (let q = 0; q < this.deviceModels.texts.length; q++) {
+            //     let text = this.deviceModels.texts[q];
+            //     text.position.x = 0;
+            //     text.position.y = 0;
+            //     text.position.z = 0;
+            // }
+        }
+
+        for (let i = 0; i < fusionList.length; i++) {
+            let d = fusionList[i];
+
+            if (d.type == 1) {
+                //平台车
+                continue;
+            }
+            // if (d.heading >=360) {
+            //     // 不处理大于360的的数据
+            //     continue;
+            // }
+            if (d.heading < 0) {
+                // 不处理小于0的的数据
+                continue;
+            }
+            if (d.targetType == 0 || d.targetType == 1 || d.targetType == 3) {
+                if (i < this.deviceModels.persons.length) {
+                    this.moveModel(d, i, "person");
                 }
-                if((count-fusionList.length)>=20)
-                {
-                    this.removeModelPrimitives();
+            } else {
+                // console.log(d.vehicleId)
+                let carModel = this.getModelForPrimitive(d.vehicleId + "car");//this.deviceModels.cars[d.vehicleId+"car"];
+                if (carModel == null) {
+                    let modelShow = this.getShowModelPrimitive();
+                    if (modelShow != null) {
+                        this.moveModel(modelShow, d);
+                    } else {
+                        //初始化增加车辆
+                        this.addModeCar(d);
+                    }
+
+
+
                 }
-                console.log(fusionList.length+"show"+count)
-                // var primitives = this.viewer.scene.primitives;
-                // var length = primitives.length;
+                else {
+                    this.moveModel(carModel, d);
+                }
 
-                // for (var k = 0; k < length; ++k) {
-                //   var p = primitives.get(k);
-                //   if (p.id == undefined) continue;
-                //   if (p.id.search("model") != -1) {
-                //     p.show = false;
+                // if (i < this.deviceModels.cars.length) {
+                //   if (d.fuselStatus == 0) {
+                //     this.moveModel(d, i, "model");
+                //     var carlabel = this.viewer.entities.getById(i + "label");
+                //     carlabel.position = Cesium.Cartesian3.fromDegrees(d.longitude, d.latitude, 5);
+                //     let h = d.heading.toFixed(1);
+                //     let s = d.speed.toFixed(1);
+                //     carlabel.show = true;
+                //     carlabel.label.text = "[" + h + ", " + s + "]";
+
                 //   }
-                // }
-
-                // var entities = this.viewer.entities;//  .getById(i + "label");
-                // var enlength = entities.values.length;
-                // for (var k = 0; k < enlength; ++k) {
-                //   var lbl = entities.values[k];
-                //   if (lbl.id == undefined) continue;
-                //   if (lbl.id.search("label") != -1) {
-                //     lbl.show = false;
-                //   }
-                // }
-                // for (var k = 0; k < length; ++k) {
-                //   var p = primitives.get(k);
-                //   if (p.id == undefined) continue;
-                //   if (p.id.search("person") != -1) {
-                //     p.show = false;
-                //   }
-                // }
-
-                // for (let p = 0; p < this.deviceModels.cars.length; p++) {
-                //   let carmodel = this.viewer.entities.getById(p + "model");
-                //   var position = Cesium.Cartesian3.fromDegrees(-1000, -1000, 0.0);
-                //   carmodel.position = position;
-                // }
-
-                // for (let q = 0; q < this.deviceModels.persons.length; q++) {
-                //     let person = this.deviceModels.persons[q];
-                //     person.position.x = 0;
-                //     person.position.y = 0;
-                //     person.position.z = 0;
-                // }
-                // for (let q = 0; q < this.deviceModels.texts.length; q++) {
-                //     let text = this.deviceModels.texts[q];
-                //     text.position.x = 0;
-                //     text.position.y = 0;
-                //     text.position.z = 0;
                 // }
             }
+        }
+        // },0); //
+    }
+    addModeCar(d)
+    {
+        var position = Cesium.Cartesian3.fromDegrees(d.longitude, d.latitude, this.defualtZ);
+        var heading = Cesium.Math.toRadians(d.heading);
+        var pitch = 0;
+        var roll = 0;
+        var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+        //判断如果等或者大于360度，设置红色
+        if (d.heading >= 360) {
+            carmodel.color = Cesium.Color.fromAlpha(Cesium.Color.RED, parseFloat(1));
+        }
+        let fixedFrameTransforms = Cesium.Transforms.localFrameToFixedFrameGenerator('north', 'west')
+        var modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(position, hpr, Cesium.Ellipsoid.WGS84, fixedFrameTransforms)
 
-            for (let i = 0; i < fusionList.length; i++) {
-                let d = fusionList[i];
-
-                if (d.type == 1) {
-                    //平台车
-                    continue;
-                }
-                // if (d.heading >=360) {
-                //     // 不处理大于360的的数据
-                //     continue;
-                // }
-                if (d.heading < 0) {
-                    // 不处理小于0的的数据
-                    continue;
-                }
-                // let dUTM = this.webMercatorUtils.lngLatToXY(d.longitude, d.latitude)
-
-                if (d.targetType == 0 || d.targetType == 1 || d.targetType == 3) {
-                    if (i < this.deviceModels.persons.length) {
-                        this.moveModel(d, i, "person");
-                    }
-                } else {
-                    // console.log(d.vehicleId)
-                    let carModel=this.getModelForPrimitive(d.vehicleId+"car");//this.deviceModels.cars[d.vehicleId+"car"];
-
-                    if(carModel==null)
-                    {
-                        let modelShow=this.getShowModelPrimitive();
-                        if(modelShow!=null)
-                        {
-                            this.moveModel(modelShow,d);
-                        }else
-                        {
-                            var position = Cesium.Cartesian3.fromDegrees(d.longitude, d.latitude, this.defualtZ);
-                            var heading = Cesium.Math.toRadians(d.heading);
-                            var pitch = 0;
-                            var roll = 0;
-                            var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
-                            //判断如果等或者大于360度，设置红色
-                            if (d.heading >= 360) {
-                                carmodel.color = Cesium.Color.fromAlpha(Cesium.Color.RED, parseFloat(1));
-                            }
-                            let fixedFrameTransforms = Cesium.Transforms.localFrameToFixedFrameGenerator('north', 'west')
-                            var modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(position, hpr, Cesium.Ellipsoid.WGS84, fixedFrameTransforms)
-
-                            let model=this.viewer.scene.primitives.add(Cesium.Model.fromGltf({
-                                id: d.vehicleId + "car",
-                                modelMatrix: modelMatrix,
-                                url: './static/model/carbox.glb',
-                                minimumPixelSize: 1,
-                                show:true,
-                                maximumScale:5,
-                                // color : Cesium.Color.fromAlpha(Cesium.Color.CHARTREUSE  , parseFloat(1)),
-                                // silhouetteColor : Cesium.Color.fromAlpha(Cesium.Color.RED, parseFloat(1)),//轮廓线
-                                colorBlendMode : Cesium.ColorBlendMode.Mix
-                                //   ,
-                                //   scale : 3.0     //放大倍数
-                                // debugWireframe:true
-                            }));
-                        }
-
-
-
-                    }
-                    else
-                    {
-                        this.moveModel(carModel,d);
-                    }
-
-                    // if (i < this.deviceModels.cars.length) {
-                    //   if (d.fuselStatus == 0) {
-                    //     this.moveModel(d, i, "model");
-                    //     var carlabel = this.viewer.entities.getById(i + "label");
-                    //     carlabel.position = Cesium.Cartesian3.fromDegrees(d.longitude, d.latitude, 5);
-                    //     let h = d.heading.toFixed(1);
-                    //     let s = d.speed.toFixed(1);
-                    //     carlabel.show = true;
-                    //     carlabel.label.text = "[" + h + ", " + s + "]";
-
-                    //   }
-                    // }
-                }
-            }
-        },50); //
+        let model = this.viewer.scene.primitives.add(Cesium.Model.fromGltf({
+            id: d.vehicleId + "car",
+            modelMatrix: modelMatrix,
+            url: './static/model/carbox.glb',
+            minimumPixelSize: 1,
+            show: true,
+            maximumScale: 5,
+            // color : Cesium.Color.fromAlpha(Cesium.Color.CHARTREUSE  , parseFloat(1)),
+            // silhouetteColor : Cesium.Color.fromAlpha(Cesium.Color.RED, parseFloat(1)),//轮廓线
+            colorBlendMode: Cesium.ColorBlendMode.Mix
+            //   ,
+            //   scale : 3.0     //放大倍数
+            // debugWireframe:true
+        }));
     }
     removeModelPrimitives() {
         var primitives = this.viewer.scene.primitives;
         for (var i = 0; i < primitives.length; i++) {
             var primitive = primitives.get(i);
-            if (primitive instanceof Cesium.Model && !primitive.show&&primitive.id.search("car") != -1) {
+            if (primitive instanceof Cesium.Model && !primitive.show && primitive.id.search("car") != -1) {
                 this.viewer.scene.primitives.remove(primitive);
             }
         }
@@ -228,7 +224,7 @@ class PerceptionCars {
         var primitives = this.viewer.scene.primitives;
         for (var i = 0; i < primitives.length; i++) {
             var primitive = primitives.get(i);
-            if (primitive instanceof Cesium.Model && !primitive.show&&primitive.id.search("car") != -1) {
+            if (primitive instanceof Cesium.Model && !primitive.show && primitive.id.search("car") != -1) {
                 return primitive;
             }
         }
@@ -243,7 +239,7 @@ class PerceptionCars {
         }
     }
     //移动模型
-    moveModel(carmodel,d) {
+    moveModel(carmodel, d) {
         var position = Cesium.Cartesian3.fromDegrees(d.longitude, d.latitude, this.defualtZ);
         var heading = Cesium.Math.toRadians(d.heading);
         var pitch = 0;
@@ -253,13 +249,20 @@ class PerceptionCars {
         //carmodel.orientation = orientation;
         carmodel.modelMatrix = orientation;
         carmodel.show = true;
-        carmodel.id=d.vehicleId+"car";
+        carmodel.id = d.vehicleId + "car";
         //判断如果等或者大于360度，设置红色
         if (d.heading >= 360) {
             carmodel.color = Cesium.Color.fromAlpha(Cesium.Color.RED, parseFloat(1));
         }
         let fixedFrameTransforms = Cesium.Transforms.localFrameToFixedFrameGenerator('north', 'west')
         Cesium.Transforms.headingPitchRollToFixedFrame(position, hpr, Cesium.Ellipsoid.WGS84, fixedFrameTransforms, carmodel.modelMatrix)
+
+        // var carlabel = this.viewer.entities.getById(i + "label");
+        // carlabel.position = Cesium.Cartesian3.fromDegrees(d.longitude, d.latitude, 5);
+        // let h = d.heading.toFixed(1);
+        // let s = d.speed.toFixed(1);
+        // carlabel.show = true;
+        // carlabel.label.text = "[" + h + ", " + s + "]";
     }
     /**
      * 初始化感知车数量
@@ -302,13 +305,6 @@ class PerceptionCars {
                     color: Cesium.Color.RED,    //点位颜色
                     pixelSize: 0          //像素点大小
                 },
-                //   box : {
-                //     dimensions : new Cesium.Cartesian3(1, 1, 1),
-                //     outline : true,
-                //     outlineColor : Cesium.Color.WHITE,
-                //     outlineWidth : 5,
-                //     material : Cesium.Color.fromRandom({alpha : 0})
-                // },
                 label: {
                     text: "",
                     fillColor: Cesium.Color.BLACK,
