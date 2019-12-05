8328997c5ac37b1c90479b947efce579ba93ab94
chiyanchao@tusvn.com <chiyanchao@tusvn.com>
2019-12-05 17:32:30 +0800

    修改地图

--- src/utils/GIS/GisData.js
+++ src/utils/GIS/GisData.js
@@ -11,19 +11,39 @@ let GisData = {
             id: "layer1",
             show: false
         }));
-        //标识（箭头）
-        viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/bs.geojson', {
-            fill: Cesium.Color.ALICEBLUE.withAlpha(1).withAlpha(0.996),//.withAlpha(1)
-            stroke: Cesium.Color.fromCssColorString('#fff').withAlpha(0.996),// Cesium.Color.ORANGE, 
-            markerSymbol: '?',
-            show: false
-        }));
+        var promise = Cesium.GeoJsonDataSource.load('static/map3d/data/bs.geojson');
+        promise.then(function (dataSource) {
+            viewer.dataSources.add(dataSource); 
+            //Get the array of entities
+            var entities = dataSource.entities.values; 
+            for (var i = 0; i < entities.length; i++) {
+                //For each entity, create a random color based on the state name.
+                //Some states have multiple entities, so we store the color in a
+                //hash so that we use the same color for the entire state.
+                var entity = entities[i]; 
+                //Set the polygon material to our random color.
+                entity.polygon.material = Cesium.Color.ALICEBLUE.withAlpha(1).withAlpha(0.996);
+                //Remove the outlines.
+                entity.polygon.outline = false;  
+            }
+        }).otherwise(function (error) {
+            //Display any errrors encountered while loading.
+            // window.alert(error);
+        });
+
+        // //标识（箭头）
+        // viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/bs.geojson', {
+        //     fill: Cesium.Color.ALICEBLUE.withAlpha(1).withAlpha(0.996),//.withAlpha(1)
+        //     // stroke: Cesium.Color.fromCssColorString('#fff').withAlpha(0.996),// Cesium.Color.ORANGE, 
+        //     // markerSymbol: '?',
+        //     show: false
+        // }));
         //绿化带
         viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/lhd.geojson', {
             fill: Cesium.Color.fromCssColorString('#758152').withAlpha(0.996),//.withAlpha(1)
             stroke: Cesium.Color.fromCssColorString('#758152').withAlpha(0.996),// Cesium.Color.ORANGE, 
-            markerSymbol: '?',
-            zIndex: 1,
+            // markerSymbol: '?',
+            // zIndex: 1,
             show: false
         }));
 
@@ -31,16 +51,16 @@ let GisData = {
         viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/bmx.geojson', {
             fill: Cesium.Color.fromCssColorString('#f3f3f3').withAlpha(0.996),
             stroke: Cesium.Color.fromCssColorString('#f3f3f3').withAlpha(0.996),// Cesium.Color.ORANGE, 
-            markerSymbol: '?',
-            zIndex: 2,
+            // markerSymbol: '?',
+            // zIndex: 2,
             show: false
         }));
         //感知区域
         viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/gzqy.geojson', {
             fill: Cesium.Color.fromCssColorString('#71446b').withAlpha(0.5),//'static/images/login-bg.jpg',//.withAlpha(1)
             stroke: Cesium.Color.fromCssColorString('#71446b').withAlpha(0.5),// Cesium.Color.ORANGE,  
-            markerSymbol: '?',
-            zIndex: 0,
+            // markerSymbol: '?',
+            // zIndex: 0,
             show: false
         }));
         //道路马路牙子
@@ -48,8 +68,8 @@ let GisData = {
         let dlwbk = viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/mlyz.geojson', {
             fill: Cesium.Color.fromCssColorString('#c0c7c5').withAlpha(0.996),//.withAlpha(1)
             stroke: Cesium.Color.fromCssColorString('#c0c7c5').withAlpha(0.996),// Cesium.Color.ORANGE, 
-            markerSymbol: '?',
-            zIndex: 1,
+            // markerSymbol: '?',
+            // zIndex: 1,
             show: false
         }));
         // dlwbk.then(function (dataSource) {
@@ -70,15 +90,15 @@ let GisData = {
         viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/dlm.geojson', {
             fill: Cesium.Color.fromCssColorString('#626669').withAlpha(1),//.withAlpha(1)
             stroke: Cesium.Color.fromCssColorString('#626669').withAlpha(1),// Cesium.Color.ORANGE, 
-            markerSymbol: '?',
-            zIndex: 0,
+            // markerSymbol: '?',
+            // zIndex: 0,
             show: false
         }));
         //道路 长虚线
         let dcdx = viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/dlcxx.geojson', {
             stroke: Cesium.Color.ALICEBLUE.withAlpha(0.996),// Cesium.Color.ORANGE, new Cesium.Color(135,75,43,1) 
             strokeWidth: 1,
-            markerSymbol: '?',
+            // markerSymbol: '?',
             show: false
         }));
         dcdx.then(function (dataSource) {
@@ -99,9 +119,9 @@ let GisData = {
 
         //道路短虚线
         let dcxx = viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/dldxx.geojson', {
-            stroke: Cesium.Color.ALICEBLUE.withAlpha(0.996),// Cesium.Color.ORANGE, new Cesium.Color(135,75,43,1)
+            // stroke: Cesium.Color.ALICEBLUE.withAlpha(0.996),// Cesium.Color.ORANGE, new Cesium.Color(135,75,43,1)
             strokeWidth: 1,
-            markerSymbol: '?',
+            // markerSymbol: '?',
             show: false
         }));
         dcxx.then(function (dataSource) {
@@ -122,15 +142,15 @@ let GisData = {
         viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/step.geojson', {
             stroke: Cesium.Color.fromCssColorString('#c0c0c0').withAlpha(0.996),// Cesium.Color.ORANGE, new Cesium.Color(135,75,43,1)
             strokeWidth: 2,
-            markerSymbol: '?',
+            // markerSymbol: '?',
             show: false
         }));
         //道路直线
         let dlzx = viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/dlzx.geojson', {
             stroke: Cesium.Color.fromCssColorString('#f3f3f3').withAlpha(0.996),// Cesium.Color.ORANGE, new Cesium.Color(135,75,43,1)
             strokeWidth: 1,
-            markerSymbol: '?',
-            zIndex: 1,
+            // markerSymbol: '?',
+            // zIndex: 1,
             show: false
         }));
         dlzx.then(function (dataSource) {
@@ -146,8 +166,8 @@ let GisData = {
         //道路外边框
         let dlbk = viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/map3d/data/dlwbk.geojson', {
             stroke: Cesium.Color.fromCssColorString('#516b03').withAlpha(0.996),// Cesium.Color.ALICEBLUE.withAlpha(0.996),
-            strokeWidth: 5,
-            markerSymbol: '?',
+            strokeWidth: 2,
+            // markerSymbol: '?',
             show: false
         }));
         dlbk.then(function (dataSource) {
@@ -176,84 +196,84 @@ let GisData = {
     },
     //初始化模型
     initModeData(viewer) {
-      
+
         this.initStreetLamp(viewer);
         let itemSide = [
-            [ 121.17598714521128, 31.28088057501294 ] ,
-            [ 121.175908342116941, 31.281047481951688 ] ,
-            [ 121.17583954051652, 31.28121453087844 ] ,
-            [ 121.175536401947809, 31.281139577864504 ] ,
-            [ 121.175612257827467, 31.280952580747613 ] ,
-            [ 121.175685991245373, 31.280818061572234 ] ,
-            [ 121.175723649439519, 31.28146015049041 ] ,
-            [ 121.175696468940473, 31.281601655743426 ] ,
-            [ 121.175664447611183, 31.281590460614954 ] ,
-            [ 121.175664522932266, 31.281629842788995 ] ,
-            [ 121.175673449548441, 31.281685819148183 ] ,
-            [ 121.175701325812014, 31.281753520985301 ] ,
-            [ 121.175737964301817, 31.281799150649082 ] ,
-            [ 121.175788104035362, 31.281836379822408 ] ,
-            [ 121.175829309765248, 31.281861309748006 ] ,
-            [ 121.175929762079093, 31.281883500994997 ] ,
-            [ 121.175428680496267, 31.281494864312084 ] ,
-            [ 121.175492968867729, 31.281331331623687 ] ,
-            [ 121.175384979402253, 31.281613938176438 ] ,
-            [ 121.175296622253455, 31.281815077913453 ] ,
-            [ 121.175222914078802, 31.282005924075772 ] ,
-            [ 121.1756217388496, 31.282076508929997 ] ,
-            [ 121.175741834864027, 31.282074395999956 ] ,
-            [ 121.175880143327305, 31.2821078652767 ] ,
-            [ 121.175479314559254, 31.282141314322701 ] ,
-            [ 121.175401685924754, 31.282247137187259 ] ,
-            [ 121.175378595494834, 31.282291679741359 ] ,
-            [ 121.175322283976158, 31.282387303762761 ] ,
-            [ 121.175239731657726, 31.282517878589541 ] ,
-            [ 121.1751763654608, 31.282114097756295 ] ,
-            [ 121.175024706903301, 31.282369710072359 ] ,
-            [ 121.174930820308688, 31.282511579956843 ] ,
-            [ 121.174976578769787, 31.282444446960984 ] ,
-            [ 121.175115424717987, 31.282219202847099 ] ,
-            [ 121.175195072147574, 31.282578463780379 ] ,
-            [ 121.175093081476149, 31.2827154451723 ] ,
-            [ 121.174866691150896, 31.282601841860235 ] ,
-            [ 121.17474068202263, 31.282759365552955 ] ,
-            [ 121.175017548435349, 31.282813421331479 ] ,
-            [ 121.174561547388492, 31.282940836730564 ] ,
-            [ 121.173472876883594, 31.283710116271362 ] ,
-            [ 121.173766087699221, 31.283810232485454 ] ,
-            [ 121.174529203986779, 31.283321299519191 ] ,
-            [ 121.174151264154787, 31.283291582524893 ] ,
-            [ 121.17266019263846, 31.284232587057836 ] ,
-            [ 121.17266919325553, 31.283982108373905 ] ,
-            [ 121.172000762643208, 31.284128927080214 ] ,
-            [ 121.170780423185619, 31.28450464631878 ] ,
-            [ 121.170801420709864, 31.284626191244545 ] ,
-            [ 121.170785388134888, 31.284708066393573 ] ,
-            [ 121.170696729683314, 31.284406075694378 ] ,
-            [ 121.17035653749744, 31.284292394877752 ] ,
-            [ 121.170365683787239, 31.284049078770582 ] ,
-            [ 121.170611479499811, 31.284100314715037 ] ,
-            [ 121.170905247449767, 31.284142686492828 ] ,
-            [ 121.171353621128816, 31.284180576169639 ] ,
-            [ 121.171141705053557, 31.284159419531221 ] ,
-            [ 121.171177965711934, 31.28446925607178 ] ,
-            [ 121.171085827581564, 31.284577733589646 ] ,
-            [ 121.171060572776568, 31.284676661904662 ] ,
-            [ 121.171402884472712, 31.284392741868281 ] ,
-            [ 121.171767116757124, 31.284384084229817 ] ,
-            [ 121.171622037735474, 31.284182965636113 ] ,
-            [ 121.170097826441022, 31.284233602719066 ] 
-            ]
-            
+            [121.17598714521128, 31.28088057501294],
+            [121.175908342116941, 31.281047481951688],
+            [121.17583954051652, 31.28121453087844],
+            [121.175536401947809, 31.281139577864504],
+            [121.175612257827467, 31.280952580747613],
+            [121.175685991245373, 31.280818061572234],
+            [121.175723649439519, 31.28146015049041],
+            [121.175696468940473, 31.281601655743426],
+            [121.175664447611183, 31.281590460614954],
+            [121.175664522932266, 31.281629842788995],
+            [121.175673449548441, 31.281685819148183],
+            [121.175701325812014, 31.281753520985301],
+            [121.175737964301817, 31.281799150649082],
+            [121.175788104035362, 31.281836379822408],
+            [121.175829309765248, 31.281861309748006],
+            [121.175929762079093, 31.281883500994997],
+            [121.175428680496267, 31.281494864312084],
+            [121.175492968867729, 31.281331331623687],
+            [121.175384979402253, 31.281613938176438],
+            [121.175296622253455, 31.281815077913453],
+            [121.175222914078802, 31.282005924075772],
+            [121.1756217388496, 31.282076508929997],
+            [121.175741834864027, 31.282074395999956],
+            [121.175880143327305, 31.2821078652767],
+            [121.175479314559254, 31.282141314322701],
+            [121.175401685924754, 31.282247137187259],
+            [121.175378595494834, 31.282291679741359],
+            [121.175322283976158, 31.282387303762761],
+            [121.175239731657726, 31.282517878589541],
+            [121.1751763654608, 31.282114097756295],
+            [121.175024706903301, 31.282369710072359],
+            [121.174930820308688, 31.282511579956843],
+            [121.174976578769787, 31.282444446960984],
+            [121.175115424717987, 31.282219202847099],
+            [121.175195072147574, 31.282578463780379],
+            [121.175093081476149, 31.2827154451723],
+            [121.174866691150896, 31.282601841860235],
+            [121.17474068202263, 31.282759365552955],
+            [121.175017548435349, 31.282813421331479],
+            [121.174561547388492, 31.282940836730564],
+            [121.173472876883594, 31.283710116271362],
+            [121.173766087699221, 31.283810232485454],
+            [121.174529203986779, 31.283321299519191],
+            [121.174151264154787, 31.283291582524893],
+            [121.17266019263846, 31.284232587057836],
+            [121.17266919325553, 31.283982108373905],
+            [121.172000762643208, 31.284128927080214],
+            [121.170780423185619, 31.28450464631878],
+            [121.170801420709864, 31.284626191244545],
+            [121.170785388134888, 31.284708066393573],
+            [121.170696729683314, 31.284406075694378],
+            [121.17035653749744, 31.284292394877752],
+            [121.170365683787239, 31.284049078770582],
+            [121.170611479499811, 31.284100314715037],
+            [121.170905247449767, 31.284142686492828],
+            [121.171353621128816, 31.284180576169639],
+            [121.171141705053557, 31.284159419531221],
+            [121.171177965711934, 31.28446925607178],
+            [121.171085827581564, 31.284577733589646],
+            [121.171060572776568, 31.284676661904662],
+            [121.171402884472712, 31.284392741868281],
+            [121.171767116757124, 31.284384084229817],
+            [121.171622037735474, 31.284182965636113],
+            [121.170097826441022, 31.284233602719066]
+        ]
+
         this.initTree(itemSide, viewer, "Htree");
 
         let itemSide2 = [[121.17551589465815, 31.281617738453047, 0.0]]
 
         this.initTree(itemSide2, viewer, "traffic_light_yellow");
-      
- 
 
-        
+
+
+
 
         //         var billboards = viewer.scene.primitives.add(new Cesium.BillboardCollection());
         // billboards.add({
@@ -337,7 +357,7 @@ let GisData = {
             }));
         }
     },
-  
+
     /**
     * 加载灯杆
     */
