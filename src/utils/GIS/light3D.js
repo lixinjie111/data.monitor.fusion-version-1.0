4be2f0911ea934bdf161efcecc926b598a442b0c
chiyanchao@tusvn.com <chiyanchao@tusvn.com>
2019-12-12 10:04:07 +0800

    修改红路灯

--- src/utils/GIS/light3D.js
+++ src/utils/GIS/light3D.js
@@ -112,8 +112,8 @@ class light3D {
             name: id + "ligth1",
             wall: {
                 positions: Cesium.Cartesian3.fromDegreesArrayHeights([
-                    x, y, 5.2,
-                    x + Math.sin(angle * (Math.PI / 180)) * 0.00002, y + Math.cos(angle * (Math.PI / 180)) * 0.00002, 5.2,
+                    x, y, 5.1,
+                    x + Math.sin(angle * (Math.PI / 180)) * 0.00002, y + Math.cos(angle * (Math.PI / 180)) * 0.00002, 5.1,
                 ]),
                 material: new Cesium.ImageMaterialProperty({
                     image: new Cesium.CallbackProperty(_img1, false),
@@ -132,8 +132,8 @@ class light3D {
             name: id + "ligth2",
             wall: {
                 positions: Cesium.Cartesian3.fromDegreesArrayHeights([
-                    x2, y2, 5.0,
-                    x2 + Math.sin(angle * (Math.PI / 180)) * 0.00001, y2 + Math.cos(angle * (Math.PI / 180)) * 0.00001, 5.0,
+                    x2, y2, 4.8,
+                    x2 + Math.sin(angle * (Math.PI / 180)) * 0.00001, y2 + Math.cos(angle * (Math.PI / 180)) * 0.00001, 4.8,
                 ]),
                 material: new Cesium.ImageMaterialProperty({
                     image: new Cesium.CallbackProperty(_img2, false),
@@ -141,7 +141,7 @@ class light3D {
                     orientation: Cesium.StripeOrientation.HORIZONTAL   //水平还是垂直，默认水平
                 }),
                 outline: false,
-                minimumHeights: [3.2, 3.2],
+                minimumHeights: [3.4,3.4],
             }
         });
 
@@ -153,8 +153,8 @@ class light3D {
             name: id + "ligth3",
             wall: {
                 positions: Cesium.Cartesian3.fromDegreesArrayHeights([
-                    x3, y3, 5.0,
-                    x3 + Math.sin(angle * (Math.PI / 180)) * 0.00001, y3 + Math.cos(angle * (Math.PI / 180)) * 0.00001, 5.0,
+                    x3, y3, 4.8,
+                    x3 + Math.sin(angle * (Math.PI / 180)) * 0.00001, y3 + Math.cos(angle * (Math.PI / 180)) * 0.00001, 4.8,
                 ]),
                 material: new Cesium.ImageMaterialProperty({
                     image: new Cesium.CallbackProperty(_img3, false),
@@ -162,7 +162,7 @@ class light3D {
                     orientation: Cesium.StripeOrientation.HORIZONTAL   //水平还是垂直，默认水平
                 }),
                 outline: false,
-                minimumHeights: [3.2, 3.2],
+                minimumHeights: [3.4,3.4],
             }
         });
 
