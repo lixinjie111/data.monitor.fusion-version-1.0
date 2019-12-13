a9adee467a058bebf1eedbc376be40bbe7bd6142
shilu <shilu@tusvn.comm>
2019-12-13 10:27:31 +0800

    单车iframe

---
+++ src/views/singleNew/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="c-map">
+        <iframe :src="iframeUrl" class="c-iframe"></iframe>
+        <right></right>
+    </div>
+</template>
+<script>
+    import Right from './components/right.vue'
+    export default {
+        data() {
+            return {
+                iframeUrl: 'http://127.0.0.1:8080/fusionMonitor/single.html?vehicleId='+this.$route.params.vehicleId+'&delayTime='+this.$route.query.delayTime
+            }
+        },
+        components:{ Right },
+        mounted() {}
+    }
+</script>
